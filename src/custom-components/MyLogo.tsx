ab857c7addbf7c9457a5404deb94f7fad627ba88
Sanjeeva Shrivastava <sanjeevashrivastava@yahoo.com>
2026-08-16 17:34:54 +0530

    initial commit

--- src/custom-components/MyLogo.tsx
+++ src/custom-components/MyLogo.tsx
@@ -2,7 +2,7 @@ import React, { memo } from 'react';
 import eq from 'lodash/eq';
 
 const MyLogo = () => {
-  return <img src="/react-mui/logo192.png" width={50} height={50} alt="logo" />;
+  return <img src="/logo192.png" width={50} height={50} alt="logo" />;
 };
 
 export default memo(MyLogo, (prevProps, nextProps) => {
